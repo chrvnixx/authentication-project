daf1d654f043c5351f33ac83f6c17cb3a379ecaf
chrvnixx <emmanueladekola91@gmail.com>
2026-01-22 15:58:41 +0100

    setup page routes

--- frontend/auth-project/src/App.jsx
+++ frontend/auth-project/src/App.jsx
@@ -1,12 +1,43 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import FloatingShape from "./components/FloatingShape";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/loginPage";
 
 function App() {
   return (
     <>
       <div
-        className="min-h-screen bg-gradient-to-br
+        className="min-h-screen bg-linear-to-br
   from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
-      ></div>
+      >
+        <FloatingShape
+          color="bg-green-500"
+          size="w-64 h-64"
+          top="5%"
+          left="10%"
+          delay={0}
+        />
+        <FloatingShape
+          color="bg-green-500"
+          size="w-48 h-48"
+          top="5%"
+          left="10%"
+          delay={0}
+        />
+        <FloatingShape
+          color="bg-green-500"
+          size="w-32 h-32"
+          top="5%"
+          left="10%"
+          delay={0}
+        />
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ frontend/auth-project/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Input({ icon: Icon, ...props }) {
+  return (
+    <div>
+      <div>
+        <Icon clasName="size-5 text-green-500" />
+      </div>
+    </div>
+  );
+}
