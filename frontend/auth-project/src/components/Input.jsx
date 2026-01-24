733260ecedb14332d2cffbf187158bc3830ab75f
chrvnixx <emmanueladekola91@gmail.com>
2026-01-24 14:45:29 +0100

    Built login ui

--- frontend/auth-project/src/components/Input.jsx
+++ frontend/auth-project/src/components/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({ icon: Icon, ...props }) {
   return (
     <div className="relative mb-6">
       <div className=" absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <Icon clasName="size-5 text-green-500" />
+        <Icon className="size-5 text-green-500" />
       </div>
       <input
         {...props}
